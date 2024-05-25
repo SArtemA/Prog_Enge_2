4d2610100afe525b9efc02bc578a3f9c09cdebea
SArtemA <148715539+SArtemA@users.noreply.github.com>
2024-05-25 22:37:17 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from typing import Union
+from fastapi import FastAPI, File, UploadFile
+
+
+
+app = FastAPI()
+
+from ultralytics import YOLO
+model = YOLO('best.pt')
+
+
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
